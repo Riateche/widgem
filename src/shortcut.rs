86b94ab1958f33f38df927b38f8369e463b5bd47
Lena Badasen <lena.badasen@gmail.com>
2023-09-07 18:39:09 +0100

    fix tmp value freed

--- src/shortcut.rs
+++ src/shortcut.rs
@@ -57,7 +57,8 @@ impl KeyCombination {
     }
 
     pub fn from_str_portable(text: &str) -> anyhow::Result<Self> {
-        let text = text.to_ascii_lowercase().trim();
+        let text = text.to_ascii_lowercase();
+        let text = text.trim();
         let mut iter = text.rsplitn(2, '+');
         let key_text = iter
             .next()
