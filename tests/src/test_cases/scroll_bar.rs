69a9f81f65846664625d16afd8ec87ae2f1cf6c5
Pavel Strakhov <ri@idzaaus.org>
2024-09-15 22:31:25 +0100

    more scroll bar tests

--- tests/src/test_cases/scroll_bar.rs
+++ tests/src/test_cases/scroll_bar.rs
@@ -107,6 +107,27 @@ pub fn check(ctx: &mut Context) -> anyhow::Result<()> {
     ctx.connection.mouse_up(1)?;
     ctx.snapshot(&mut window, "released slider - no highlight")?;
 
+    window.mouse_move(60, 20)?;
+    ctx.snapshot(&mut window, "highlighted slider")?;
+    ctx.connection.mouse_down(1)?;
+    ctx.snapshot(&mut window, "grabbed slider")?;
+    window.mouse_move(0, 20)?;
+    ctx.snapshot(&mut window, "dragged all the way left")?;
+    window.mouse_move(20, 20)?;
+    ctx.snapshot(&mut window, "still all the way left")?;
+    window.mouse_move(58, 20)?;
+    ctx.snapshot(&mut window, "no longer all the way left")?;
+    ctx.connection.mouse_up(1)?;
+    ctx.snapshot(&mut window, "released slider")?;
+
+    window.mouse_move(90, 24)?;
+    ctx.connection.mouse_down(1)?;
+    ctx.snapshot(&mut window, "grabbed slider")?;
+    window.mouse_move(300, 24)?;
+    ctx.snapshot(&mut window, "dragged all the way right")?;
+    ctx.connection.mouse_up(1)?;
+    ctx.snapshot(&mut window, "released slider - no highlight")?;
+
     window.close()?;
     Ok(())
 }
