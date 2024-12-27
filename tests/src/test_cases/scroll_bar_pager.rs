78b5765c10b7e5f11d6584f58aefdd6c98344307
Pavel Strakhov <ri@idzaaus.org>
2024-12-27 11:59:30 +0000

    test scroll bar auto repeat

--- tests/src/test_cases/scroll_bar_pager.rs
+++ tests/src/test_cases/scroll_bar_pager.rs
@@ -1,3 +1,5 @@
+use std::{thread::sleep, time::Duration};
+
 use crate::context::Context;
 
 pub use super::scroll_bar::RootWidget;
@@ -23,6 +25,26 @@ pub fn check(ctx: &mut Context) -> anyhow::Result<()> {
     ctx.snapshot(&mut window, "page left")?;
     window.mouse_move(0, 0)?;
     ctx.snapshot(&mut window, "no highlight")?;
+
+    ctx.connection.key("1")?;
+    ctx.snapshot(&mut window, "increase range")?;
+    window.mouse_move(100, 20)?;
+    ctx.snapshot(&mut window, "highlighted pager")?;
+
+    ctx.connection.mouse_down(1)?;
+    ctx.snapshot(&mut window, "page right")?;
+    sleep(Duration::from_millis(700)); // auto repeat delay is 2 s; snapshot delay is 0.5 s
+    ctx.connection.mouse_up(1)?;
+    ctx.snapshot(&mut window, "released pager - no auto repeat")?;
+
+    ctx.connection.mouse_down(1)?;
+    ctx.snapshot(&mut window, "page right")?;
+    sleep(Duration::from_millis(1300));
+    ctx.snapshot(&mut window, "page right - first auto repeat")?;
+    sleep(Duration::from_millis(500));
+    ctx.snapshot(&mut window, "page right - second auto repeat")?;
+    ctx.connection.mouse_up(1)?;
+    ctx.snapshot(&mut window, "released pager - no more auto repeats")?;
 
     window.close()?;
     Ok(())
