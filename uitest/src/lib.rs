7839cf41950ca9464a98e57329f9391240a3f48f
Pavel Strakhov <ri@idzaaus.org>
2025-09-14 10:03:22 +0100

    uitest: create window module

--- uitest/src/lib.rs
+++ uitest/src/lib.rs
@@ -2,6 +2,10 @@ mod linux;
 
 use self::linux as imp;
 
+mod window;
+
+pub use crate::window::Window;
+
 use {
     anyhow::bail,
     std::{
@@ -9,7 +13,6 @@ use {
         thread::sleep,
         time::{Duration, Instant},
     },
-    xcap::{image::RgbaImage, XCapResult},
 };
 
 const SINGLE_WAIT_DURATION: Duration = Duration::from_millis(200);
@@ -41,7 +44,7 @@ impl Connection {
 
     pub fn windows_by_pid(&self, pid: u32) -> anyhow::Result<Vec<Window>> {
         let windows = self.all_windows()?;
-        Ok(windows.into_iter().filter(|w| w.pid == pid).collect())
+        Ok(windows.into_iter().filter(|w| w.pid() == pid).collect())
     }
 
     pub fn wait_for_windows_by_pid(
@@ -126,90 +129,5 @@ impl Connection {
 
     pub fn mouse_move_global(&self, x: u32, y: u32) -> anyhow::Result<()> {
         self.0.imp.mouse_move_global(x, y)
-    }
-}
-
-#[derive(Clone)]
-pub struct Window {
-    id: u32,
-    pid: u32,
-    inner: xcap::Window,
-    #[allow(dead_code)]
-    connection: Connection,
-}
-
-impl Window {
-    fn new(connection: Connection, inner: xcap::Window) -> anyhow::Result<Self> {
-        Ok(Self {
-            id: inner.id()?,
-            pid: inner.pid()?,
-            inner,
-            connection,
-        })
-    }
-
-    pub fn pid(&self) -> u32 {
-        self.pid
-    }
-
-    /// The window id
-    pub fn id(&self) -> u32 {
-        self.id
-    }
-    /// The window app name
-    pub fn app_name(&self) -> XCapResult<String> {
-        self.inner.app_name()
-    }
-    /// The window title
-    pub fn title(&self) -> XCapResult<String> {
-        self.inner.title()
-    }
-    /// The window x coordinate.
-    pub fn x(&self) -> XCapResult<i32> {
-        self.inner.x()
-    }
-    /// The window x coordinate.
-    pub fn y(&self) -> XCapResult<i32> {
-        self.inner.y()
-    }
-    /// The window pixel width.
-    pub fn width(&self) -> XCapResult<u32> {
-        self.inner.width()
-    }
-    /// The window pixel height.
-    pub fn height(&self) -> XCapResult<u32> {
-        self.inner.height()
-    }
-    /// The window is minimized.
-    pub fn is_minimized(&self) -> XCapResult<bool> {
-        self.inner.is_minimized()
-    }
-    /// The window is maximized.
-    pub fn is_maximized(&self) -> XCapResult<bool> {
-        self.inner.is_maximized()
-    }
-
-    pub fn capture_image(&self) -> anyhow::Result<RgbaImage> {
-        Ok(self.inner.capture_image()?)
-    }
-
-    pub fn activate(&self) -> anyhow::Result<()> {
-        self.connection.0.imp.activate_window(self)
-    }
-
-    pub fn mouse_move(&self, x: u32, y: u32) -> anyhow::Result<()> {
-        self.connection.0.imp.mouse_move(self, x, y)
-    }
-
-    pub fn minimize(&self) -> anyhow::Result<()> {
-        self.connection.0.imp.minimize_window(self)
-    }
-
-    pub fn close(&self) -> anyhow::Result<()> {
-        self.connection.0.imp.close_window(self)
-    }
-
-    pub fn resize(&self, width: i32, height: i32) -> anyhow::Result<()> {
-        self.connection.0.imp.resize_window(self, width, height)
     }
 }
---
+++ uitest/src/window.rs
@@ -0,0 +1,88 @@
+use xcap::{image::RgbaImage, XCapResult};
+
+use crate::Connection;
+
+#[derive(Clone)]
+pub struct Window {
+    id: u32,
+    pid: u32,
+    inner: xcap::Window,
+    #[allow(dead_code)]
+    connection: Connection,
+}
+
+impl Window {
+    pub(crate) fn new(connection: Connection, inner: xcap::Window) -> anyhow::Result<Self> {
+        Ok(Self {
+            id: inner.id()?,
+            pid: inner.pid()?,
+            inner,
+            connection,
+        })
+    }
+
+    pub fn pid(&self) -> u32 {
+        self.pid
+    }
+
+    /// The window id
+    pub fn id(&self) -> u32 {
+        self.id
+    }
+    /// The window app name
+    pub fn app_name(&self) -> XCapResult<String> {
+        self.inner.app_name()
+    }
+    /// The window title
+    pub fn title(&self) -> XCapResult<String> {
+        self.inner.title()
+    }
+    /// The window x coordinate.
+    pub fn x(&self) -> XCapResult<i32> {
+        self.inner.x()
+    }
+    /// The window x coordinate.
+    pub fn y(&self) -> XCapResult<i32> {
+        self.inner.y()
+    }
+    /// The window pixel width.
+    pub fn width(&self) -> XCapResult<u32> {
+        self.inner.width()
+    }
+    /// The window pixel height.
+    pub fn height(&self) -> XCapResult<u32> {
+        self.inner.height()
+    }
+    /// The window is minimized.
+    pub fn is_minimized(&self) -> XCapResult<bool> {
+        self.inner.is_minimized()
+    }
+    /// The window is maximized.
+    pub fn is_maximized(&self) -> XCapResult<bool> {
+        self.inner.is_maximized()
+    }
+
+    pub fn capture_image(&self) -> anyhow::Result<RgbaImage> {
+        Ok(self.inner.capture_image()?)
+    }
+
+    pub fn activate(&self) -> anyhow::Result<()> {
+        self.connection.0.imp.activate_window(self)
+    }
+
+    pub fn mouse_move(&self, x: u32, y: u32) -> anyhow::Result<()> {
+        self.connection.0.imp.mouse_move(self, x, y)
+    }
+
+    pub fn minimize(&self) -> anyhow::Result<()> {
+        self.connection.0.imp.minimize_window(self)
+    }
+
+    pub fn close(&self) -> anyhow::Result<()> {
+        self.connection.0.imp.close_window(self)
+    }
+
+    pub fn resize(&self, width: i32, height: i32) -> anyhow::Result<()> {
+        self.connection.0.imp.resize_window(self, width, height)
+    }
+}
